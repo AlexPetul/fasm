a1bfe8142ccee9aab5b8b955e8ddb30db2f93378
Alexey Petul <lexapetulcsgogo@mail.ru>
2023-12-22 11:12:22 +0400

    Add files via upload

--- fasm/tests/factories.py
+++ fasm/tests/factories.py
@@ -6,20 +6,15 @@ from sqlalchemy import (
     create_engine,
 )
 from sqlalchemy.orm import (
-    declarative_base,
     scoped_session,
     sessionmaker,
 )
 from sqlalchemy.pool import NullPool
 
 from src.auth.models import User
-from src.db.config import (
-    async_session,
-    get_session,
-)
 from src.sections.models import Section
 
-engine = create_engine("postgresql+asyncpg://postgres:postgres@postgres:5432/postgres", echo=False, poolclass=NullPool)
+engine = create_engine("postgresql://postgres:postgres@postgres:5432/postgres", echo=False, poolclass=NullPool)
 session = scoped_session(sessionmaker(bind=engine))
 
 
@@ -27,9 +22,11 @@ class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = User
         sqlalchemy_session = session
+        sqlalchemy_session_persistence = "commit"
 
 
 class SectionFactory(factory.alchemy.SQLAlchemyModelFactory):
     class Meta:
         model = Section
         sqlalchemy_session = session
+        sqlalchemy_session_persistence = "commit"
