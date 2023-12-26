6cf30e5ba791c5a83bae50d18c584f5b2007df9d
Petul Alexey <a.petul@icode.by>
2023-12-26 11:18:45 +0400

    #

--- fasm/src/db/config.py
+++ fasm/src/db/config.py
@@ -11,6 +11,8 @@ from sqlalchemy.orm import (
 )
 from sqlalchemy.pool import NullPool
 
+from src.settings import get_settings
+
 POSTGRES_INDEXES_NAMING_CONVENTION = {
     "ix": "%(column_0_label)s_idx",
     "uq": "%(table_name)s_%(column_0_name)s_key",
@@ -19,9 +21,11 @@ POSTGRES_INDEXES_NAMING_CONVENTION = {
     "pk": "%(table_name)s_pkey",
 }
 
+settings = get_settings()
+
 Base = declarative_base(metadata=MetaData(naming_convention=POSTGRES_INDEXES_NAMING_CONVENTION))
 
-async_engine = create_async_engine(os.getenv("DATABASE_URL"), echo=False, poolclass=NullPool)
+async_engine = create_async_engine(settings.database_url, echo=False, poolclass=NullPool)
 async_session = sessionmaker(async_engine, autoflush=True, expire_on_commit=False, class_=AsyncSession)
 
 
--- fasm/src/settings.py
+++ fasm/src/settings.py
@@ -1,10 +1,21 @@
 from functools import lru_cache
 
-from pydantic import SecretStr
+from pydantic import (
+    PostgresDsn,
+    SecretStr,
+)
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    env_file: str = ".env"
+
+    db_user: str
+    db_name: str
+    db_password: str
+    db_host: str = "postgres"
+    db_driver: str = "postgresql+asyncpg"
+
     aws_cognito_region: str
     aws_access_key_id: SecretStr
     aws_secret_access_key: SecretStr
@@ -12,6 +23,17 @@ class Settings(BaseSettings):
     aws_cognito_user_pool: SecretStr
 
     openai_api_key: SecretStr
+
+    @property
+    def database_url(self):
+        dsn = PostgresDsn.build(
+            scheme=self.db_driver,
+            host=self.db_host,
+            path=self.db_name,
+            username=self.db_user,
+            password=self.db_password,
+        )
+        return str(dsn)
 
 
 @lru_cache
